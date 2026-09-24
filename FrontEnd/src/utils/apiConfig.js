5d2b5bf1703212cec8c54d7255e7e41d7a376b83
keys-3 <140890501+Keys-3@users.noreply.github.com>
2026-09-24 18:25:51 +0530

    Update apiConfig.js

--- FrontEnd/src/utils/apiConfig.js
+++ FrontEnd/src/utils/apiConfig.js
@@ -6,14 +6,14 @@ export const getBackendUrl = () => {
     return process.env.EXPO_PUBLIC_BACKEND_URL;
   }
 
-  const hostUri = Constants?.expoConfig?.hostUri;
-  if (hostUri) {
-    return `http://${hostUri.split(':')[0]}:3000`;
-  }
-
   // If not in development, default to production backend
   if (!__DEV__) {
     return 'https://hygrow.onrender.com';
+  }
+
+  const hostUri = Constants?.expoConfig?.hostUri;
+  if (hostUri) {
+    return `http://${hostUri.split(':')[0]}:3000`;
   }
 
   if (Platform.OS === 'web') {
